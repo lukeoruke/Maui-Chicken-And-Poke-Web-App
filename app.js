4fba33c8c6b95c9830a052eccea5c4b8dc3a440f
Larry James <larryjdel@gmail.com>
2022-02-28 20:47:21 -0800

    small change

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const menu = [
   {
     id: 1,
-    title: "buttermilk pancakes",
-    category: "breakfast",
-    price: 15.99,
+    title: "Teriyaki Chicken",
+    category: "lunch",
+    price: 7.99,
     img: "./images/item-1.jpeg",
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
     id: 2,
-    title: "diner double",
+    title: "Garlic Shrimp",
     category: "lunch",
-    price: 13.99,
+    price: 10.99,
     img: "./images/item-2.jpeg",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
@@ -119,7 +119,7 @@ function displayMenuButtons() {
       }
       return values;
     },
-    ["all"]
+    ["all","other"]
   );
   const categoryBtns = categories
     .map(function (category) {
